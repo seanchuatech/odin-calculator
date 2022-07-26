91e4fa69c2616b68e969768e43dd4b21b3b45126
Leonard Sean <seanchuatech@gmail.com>
2022-07-26 19:21:21 +0800

    damn the final project

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let operator = "";
 const currentDisplayNumber = document.querySelector(".currentNumber");
 const previousDisplayNumber = document.querySelector(".previousNumber");
 
+window.addEventListener('keydown', handleKeyPress);
+
 const equal = document.querySelector(".equal");
 equal.addEventListener("click", () => {
     if(currentNum != "" && previousNum != "") {
@@ -13,6 +15,9 @@ equal.addEventListener("click", () => {
 });
 
 const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", () => {
+    addDecimal();
+});
 
 const clear = document.querySelector(".clear");
 clear.addEventListener("click", clearCalculator);
@@ -29,6 +34,10 @@ numberButtons.forEach((btn) => {
 });
 
 function handleNumber(number) {
+    if(previousNum !== "" && currentNum !=="" && operator === "") {
+        previousNum = "";
+        currentDisplayNumber.textContent = currentNum;
+    }
     if (currentNum.length <= 11) {
         currentNum += number;
         currentDisplayNumber.textContent = currentNum;
@@ -42,11 +51,24 @@ operators.forEach((btn) => {
 });
 
 function handleOperator(op) {
-    operator = op;
-    previousNum = currentNum;
-    previousDisplayNumber.textContent = previousNum + operator;
+    if (previousNum === "") {
+        previousNum = currentNum;
+        operatorCheck(op);
+    } else if (currentNum === "") {
+        operatorCheck(op);
+    } else {
+        calculate();
+        operator = op;
+        currentDisplayNumber.textContent = "0";
+        previousDisplayNumber.textContent = previousNum + " " + operator;
+    }
+}
+
+function operatorCheck(text) {
+    operator = text;
+    previousDisplayNumber.textContent = previousNum + " " + operator;
+    currentDisplayNumber.textContent = "0";
     currentNum = "";
-    currentDisplayNumber.textContent = "";
 }
 
 function calculate() {
@@ -62,28 +84,30 @@ function calculate() {
     } else if (operator === "/") {
         if (currentNum <= 0) {
             previousNum = "Error";
-            dispplayResult();
+            displayResults();
             return;
         }
         previousNum /= currentNum;
     }
     previousNum = roundNumber(previousNum);
     previousNum = previousNum.toString();
-    dispplayResult();
+    displayResults();
 }
 
 function roundNumber(num) {
     return Math.round(num * 100000) / 100000;
 }
 
-function dispplayResult() {
-    previousDisplayNumber.textContent = "";
-    operator = "";
+function displayResults() {
+    
     if(previousNum.length <= 11) {
         currentDisplayNumber.textContent = previousNum;
     } else {
         currentDisplayNumber.textContent = previousNum.slice(0, 11) + "...";
     }
+    previousDisplayNumber.textContent = "";
+    operator = "";
+    currentNum = ""; 
 }
 
 function clearCalculator() {
@@ -93,3 +117,46 @@ function clearCalculator() {
     currentDisplayNumber.textContent = "0";
     previousDisplayNumber.textContent = "";
 }
+
+function addDecimal() {
+    if(!currentNum.includes('.')) {
+        currentNum += ".";
+        currentDisplayNumber.textContent = currentNum;
+    }
+}
+
+function handleKeyPress(e) {
+    e.preventDefault();
+    if (e.key >= 0 && e.key <= 9) {
+        handleNumber(e.key);
+    } 
+    if (e.key === "Enter" || (e.key === "=" && currentNum != "" && previousNum != "")) {
+        compute();
+    }
+    if(e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+        handleOperator(e.key);
+    }
+    if(e.key === ".") {
+        addDecimal();
+    }
+    if(e.key === "Backspace") {
+        handleDelete();
+    }
+}
+
+function handleDelete() {
+    if(currentNum != "") {
+        currentNum = currentNum.slice(0, -1);
+        currentDisplayNumber.textContent = currentNum;
+        if(currentNum === "") {
+            currentDisplayNumber.textContent = "0";
+        }
+    }
+    if(currentNum === "" && previousNum !== "" && operator === "") {
+        previousNum = previousNum.slice(0, -1);
+        currentDisplayNumber.textContent = previousNum;
+    }
+    
+}
+
+//https://www.youtube.com/watch?v=2L30sFBxUyw&t=437s
